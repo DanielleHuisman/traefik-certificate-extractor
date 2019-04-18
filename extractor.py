9eb56d2b4f1d93c74b1ecdc83e35d6204d57c57d
Daniel Huisman <daniel@huisman.me>
2019-04-18 11:50:39 +0200

    Improve initial extraction

--- extractor.py
+++ extractor.py
@@ -3,103 +3,106 @@ import os
 import errno
 import time
 import json
+import glob
 from base64 import b64decode
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
-        self.handleEvent(event)
+        self.handle_event(event)
 
     def on_modified(self, event):
-        self.handleEvent(event)
+        self.handle_event(event)
 
-    def handleEvent(self, event):
+    def handle_event(self, event):
         # Check if it's a JSON file
         if not event.is_directory and event.src_path.endswith('.json'):
-            print('Certificates changed')
-            self.handleFile(event.src_path)
+            print('Certificate storage changed (' + os.path.basename(event.src_path) + ')')
+            self.handle_file(event.src_path)
 
-    def handleFile(self, file):
-            # Read JSON file
-            data = json.loads(open(file).read())
+    def handle_file(self, file):
+        # Read JSON file
+        data = json.loads(open(file).read())
 
-            # Determine ACME version
-            try:
-                acme_version = 2 if 'acme-v02' in data['Account']['Registration']['uri'] else 1
-            except TypeError:
-                if 'DomainsCertificate' in data:
-                    acme_version = 1
-                else:
-                    acme_version = 2
+        # Determine ACME version
+        try:
+            acme_version = 2 if 'acme-v02' in data['Account']['Registration']['uri'] else 1
+        except TypeError:
+            if 'DomainsCertificate' in data:
+                acme_version = 1
+            else:
+                acme_version = 2
 
-            # Find certificates
+        # Find certificates
+        if acme_version == 1:
+            certs = data['DomainsCertificate']['Certs']
+        elif acme_version == 2:
+            certs = data['Certificates']
+
+        print('Certificate storage contains ' + str(len(certs)) + ' certificates')
+
+        # Loop over all certificates
+        for c in certs:
             if acme_version == 1:
-                certs = data['DomainsCertificate']['Certs']
+                name = c['Certificate']['Domain']
+                privatekey = c['Certificate']['PrivateKey']
+                fullchain = c['Certificate']['Certificate']
+                sans = c['Domains']['SANs']
             elif acme_version == 2:
-                certs = data['Certificates']
+                name = c['Domain']['Main']
+                privatekey = c['Key']
+                fullchain = c['Certificate']
+                sans = c['Domain']['SANs']
 
-            # Loop over all certificates
-            for c in certs:
-                if acme_version == 1:
-                    name = c['Certificate']['Domain']
-                    privatekey = c['Certificate']['PrivateKey']
-                    fullchain = c['Certificate']['Certificate']
-                    sans = c['Domains']['SANs']
-                elif acme_version == 2:
-                    name = c['Domain']['Main']
-                    privatekey = c['Key']
-                    fullchain = c['Certificate']
-                    sans = c['Domain']['SANs']
+            # Decode private key, certificate and chain
+            privatekey = b64decode(privatekey).decode('utf-8')
+            fullchain = b64decode(fullchain).decode('utf-8')
+            start = fullchain.find('-----BEGIN CERTIFICATE-----', 1)
+            cert = fullchain[0:start]
+            chain = fullchain[start:]
 
-                # Decode private key, certificate and chain
-                privatekey = b64decode(privatekey).decode('utf-8')
-                fullchain = b64decode(fullchain).decode('utf-8')
-                start = fullchain.find('-----BEGIN CERTIFICATE-----', 1)
-                cert = fullchain[0:start]
-                chain = fullchain[start:]
+            # Create domain directory if it doesn't exist
+            directory = 'certs/' + name + '/'
+            try:
+                os.makedirs(directory)
+            except OSError as error:
+                if error.errno != errno.EEXIST:
+                    raise
 
-                # Create domain directory if it doesn't exist
-                directory = 'certs/' + name + '/'
-                try:
-                    os.makedirs(directory)
-                except OSError as error:
-                    if error.errno != errno.EEXIST:
-                        raise
+            # Write private key, certificate and chain to file
+            with open(directory + 'privkey.pem', 'w') as f:
+                f.write(privatekey)
 
-                # Write private key, certificate and chain to file
-                with open(directory + 'privkey.pem', 'w') as f:
-                    f.write(privatekey)
+            with open(directory + 'cert.pem', 'w') as f:
+                f.write(cert)
 
-                with open(directory + 'cert.pem', 'w') as f:
-                    f.write(cert)
+            with open(directory + 'chain.pem', 'w') as f:
+                f.write(chain)
 
-                with open(directory + 'chain.pem', 'w') as f:
-                    f.write(chain)
+            with open(directory + 'fullchain.pem', 'w') as f:
+                f.write(fullchain)
 
-                with open(directory + 'fullchain.pem', 'w') as f:
-                    f.write(fullchain)
+            # Write private key, certificate and chain to flat files
+            directory = 'certs_flat/'
 
-                # Write private key, certificate and chain to flat files
-                directory = 'certs_flat/'
+            with open(directory + name + '.key', 'w') as f:
+                f.write(privatekey)
+            with open(directory + name + '.crt', 'w') as f:
+                f.write(fullchain)
+            with open(directory + name + '.chain.pem', 'w') as f:
+                f.write(chain)
 
-                with open(directory + name + '.key', 'w') as f:
-                    f.write(privatekey)
-                with open(directory + name + '.crt', 'w') as f:
-                    f.write(fullchain)
-                with open(directory + name + '.chain.pem', 'w') as f:
-                    f.write(chain)
+            if sans:
+                for name in sans:
+                    with open(directory + name + '.key', 'w') as f:
+                        f.write(privatekey)
+                    with open(directory + name + '.crt', 'w') as f:
+                        f.write(fullchain)
+                    with open(directory + name + '.chain.pem', 'w') as f:
+                        f.write(chain)
 
-                if sans:
-                    for name in sans:
-                        with open(directory + name + '.key', 'w') as f:
-                            f.write(privatekey)
-                        with open(directory + name + '.crt', 'w') as f:
-                            f.write(fullchain)
-                        with open(directory + name + '.chain.pem', 'w') as f:
-                            f.write(chain)
-
-                print('Extracted certificate for: ' + name + (', ' + ', '.join(sans) if sans else ''))
+            print('Extracted certificate for: ' + name + (', ' + ', '.join(sans) if sans else ''))
 
 if __name__ == "__main__":
     # Determine path to watch
@@ -121,13 +124,14 @@ if __name__ == "__main__":
     event_handler = Handler()
     observer = Observer()
 
-    # Initial extract
-    if not os.listdir('certs_flat'):
-        acme = path + '/acme.json'
-        if os.path.isfile(acme):
-            event_handler.handleFile(acme)
-        else:
-            print('acme.json not found - skipping initial update')
+    # Extract certificates from current file(s) before watching
+    files = glob.glob(os.path.join(path, '*.json'))
+    try:
+        for file in files:
+            print('Certificate storage found (' + os.path.basename(file) + ')')
+            event_handler.handle_file(file)
+    except Exception as e:
+        print(e)
 
     # Register the directory to watch
     observer.schedule(event_handler, path)
